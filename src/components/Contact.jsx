35d7764d8619c14e5d3350bbe54b0de21900c51e
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-10-24 17:32:11 -0400

    Finalized part 3 changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
-export default function Contact(props) {
+export default function Contact({type, info}) {
     return (
         <div className='contact'>
             <div>
-                <h3 className='type'>{props.type || "Contact Method"}</h3>
-                <p className='number gray'>{props.info || "Contact Info"}</p>
+                <h3 className='type'>{type || "Contact Method"}</h3>
+                <p className='number gray'>{info || "Contact Info"}</p>
             </div>
             <div>
                 <p className="arrow">{">"}</p>
